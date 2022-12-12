782ce77c3ce7a5f029e5ef99b8116a43a7f2f3bf
Melody-Le <tanmui1992@gmail.com>
2022-12-13 00:44:52 +0800

    feat: update backend link

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
-const BASE_URL = 'http://localhost:8800/api/v1';
+import axios from "axios";
+const BASE_URL = "https://hello-world-be.onrender.com/api/v1";
 
 export default axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { "Content-Type": "application/json" },
 });
